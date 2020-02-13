8239b42a58073e2cdc6c7b0c6bf4dd301521f74d
thomas <thomasmckanna@gmail.com>
2020-02-12 20:58:35 -0600

    Added generic packages and the three core activities.

---
+++ TheRollaMissionApp/app/src/main/java/com/mckanna/therollamissionapp/ui/checkin/CheckInActivity.kt
@@ -0,0 +1,13 @@
+package com.mckanna.therollamissionapp.ui.checkin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mckanna.therollamissionapp.R
+
+class CheckInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkin)
+    }
+}
---
+++ TheRollaMissionApp/app/src/main/java/com/mckanna/therollamissionapp/ui/registration/RegistationActivity.kt
@@ -0,0 +1,13 @@
+package com.mckanna.therollamissionapp.ui.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mckanna.therollamissionapp.R
+
+class RegistationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registation)
+    }
+}
