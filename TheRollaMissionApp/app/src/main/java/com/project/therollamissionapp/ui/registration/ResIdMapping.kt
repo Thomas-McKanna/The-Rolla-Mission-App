add8ee16b415e8c8bd917c2cf3c59724eb240980
thomas <thomasmckanna@gmail.com>
2020-02-27 10:24:47 -0600

    Simplified a utility class.

--- TheRollaMissionApp/app/src/main/java/com/project/therollamissionapp/ui/registration/ResIdMapping.kt
+++ TheRollaMissionApp/app/src/main/java/com/project/therollamissionapp/ui/registration/ResIdMapping.kt
@@ -2,64 +2,62 @@ package com.project.therollamissionapp.ui.registration
 
 import com.project.therollamissionapp.R
 
-class ResIdMapping {
-    companion object {
-        fun getGender(id: Int): String {
-            return when (id) {
-                R.id.radio_gender_male -> "Male"
-                R.id.radio_gender_female -> "Female"
-                R.id.radio_gender_other -> "Other"
-                R.id.radio_gender_no_ans -> "N/A"
-                else -> ""
-            }
+object ResIdMapping {
+    fun getGender(id: Int): String {
+        return when (id) {
+            R.id.radio_gender_male -> "Male"
+            R.id.radio_gender_female -> "Female"
+            R.id.radio_gender_other -> "Other"
+            R.id.radio_gender_no_ans -> "N/A"
+            else -> ""
         }
+    }
 
-        fun getReasonRolla(id: Int): String {
-            return when (id) {
-                R.id.radio_reason_family -> "Family in the area"
-                R.id.radio_reason_work -> "Work/job offer"
-                R.id.radio_reason_school -> "School"
-                R.id.radio_reason_relationship -> "Relationship"
-                R.id.radio_reason_hospital -> "Hospital/Medical care"
-                R.id.radio_reason_mission -> "The Rolla Mission"
-                R.id.radio_reason_other -> "Other"
-                else -> ""
-            }
+    fun getReasonRolla(id: Int): String {
+        return when (id) {
+            R.id.radio_reason_family -> "Family in the area"
+            R.id.radio_reason_work -> "Work/job offer"
+            R.id.radio_reason_school -> "School"
+            R.id.radio_reason_relationship -> "Relationship"
+            R.id.radio_reason_hospital -> "Hospital/Medical care"
+            R.id.radio_reason_mission -> "The Rolla Mission"
+            R.id.radio_reason_other -> "Other"
+            else -> ""
         }
+    }
 
-        fun getTimeHomeless(id: Int): String {
-            return when (id) {
-                R.id.radio_homeless_1to3month -> "1-3 months"
-                R.id.radio_homeless_4to6month -> "4-6 months"
-                R.id.radio_homeless_6to12month -> "6-12 months"
-                R.id.radio_homeless_1to2year -> "1-2 years"
-                R.id.radio_homeless_2plusyear -> "2+ years"
-                else -> ""
-            }
+    fun getTimeHomeless(id: Int): String {
+        return when (id) {
+            R.id.radio_homeless_1to3month -> "1-3 months"
+            R.id.radio_homeless_4to6month -> "4-6 months"
+            R.id.radio_homeless_6to12month -> "6-12 months"
+            R.id.radio_homeless_1to2year -> "1-2 years"
+            R.id.radio_homeless_2plusyear -> "2+ years"
+            else -> ""
         }
+    }
 
-        fun getVeteran(id: Int): Boolean {
-            return when (id) {
-                R.id.radio_veteran_y -> true
-                R.id.radio_veteran_n -> false
-                else -> false
-            }
+    fun getVeteran(id: Int): Boolean {
+        return when (id) {
+            R.id.radio_veteran_y -> true
+            R.id.radio_veteran_n -> false
+            else -> false
         }
+    }
 
-        fun getViolence(id: Int): Boolean {
-            return when (id) {
-                R.id.radio_violence_y -> true
-                R.id.radio_violence_n -> false
-                else -> false
-            }
+    fun getViolence(id: Int): Boolean {
+        return when (id) {
+            R.id.radio_violence_y -> true
+            R.id.radio_violence_n -> false
+            else -> false
         }
+    }
 
-        fun getOffender(id: Int): Boolean {
-            return when (id) {
-                R.id.radio_offender_y -> true
-                R.id.radio_offender_n -> false
-                else -> false
-            }
+    fun getOffender(id: Int): Boolean {
+        return when (id) {
+            R.id.radio_offender_y -> true
+            R.id.radio_offender_n -> false
+            else -> false
         }
     }
 }
