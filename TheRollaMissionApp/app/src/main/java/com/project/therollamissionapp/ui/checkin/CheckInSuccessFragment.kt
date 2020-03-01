edac51f37f20ab1455727d80e2d39e3f4b9ee4e9
thomas <thomasmckanna@gmail.com>
2020-02-29 21:25:27 -0600

    Implemented check-in success fragment.

--- TheRollaMissionApp/app/src/main/java/com/project/therollamissionapp/ui/checkin/CheckInSuccessFragment.kt
+++ TheRollaMissionApp/app/src/main/java/com/project/therollamissionapp/ui/checkin/CheckInSuccessFragment.kt
@@ -5,20 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.project.therollamissionapp.R
+import com.project.therollamissionapp.databinding.FragmentCheckInSuccessBinding
+import kotlinx.coroutines.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class CheckInSuccessFragment : Fragment() {
+    lateinit var binding: FragmentCheckInSuccessBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_in_success, container, false)
+        val root = inflater.inflate(R.layout.fragment_check_in_success, container, false)
+        binding = FragmentCheckInSuccessBinding.bind(root)
+
+        binding.buttonReturnStart.setOnClickListener {
+            val action = CheckInSuccessFragmentDirections.actionGlobalWelcomeFragment()
+            findNavController().navigate(action)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            // Automatically return to start screen after 10 seconds
+            returnToStartAfterDelay(10000)
+        }
+
+        return binding.root
     }
 
+    private suspend fun returnToStartAfterDelay(timeMillis: Long) {
+        delay(timeMillis)
+        binding.buttonReturnStart.callOnClick()
+    }
 }
